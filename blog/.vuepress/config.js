42de8643700918198642e5e4ebfeeb7a9a718c1a
lixin28 <lixin28@100tal.com>
2020-04-25 20:37:47 +0800

    ssr seo

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,7 +1,10 @@
 
 module.exports = {
   title: 'MozLee',
-  description: 'MozLee,瞅啥那',
+  description: 'MozLee,瞅啥那,前端开发工程师,mozlee|首页MozLee的博客',
+  head:[
+    ['meta',{name:'keywords',content:"MozLee,mozlee,博客,web前端工程师,攻城狮,技术分享,资源分享"}]
+  ],
   dest: './web',
   themeConfig: {
     info:{
